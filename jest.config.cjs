217d1582e8143bcdc801ff69361ede28607f1724
Mountee <mountee@msn.com>
2024-11-26 17:40:08 +0000

    more tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,28 +1,17 @@
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest'
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleNameMapper: {
     '^src/(.*)$': '<rootDir>/src/$1',
     '\\.json$': '<rootDir>/src/__mocks__/jsonMock.js',
     '^../utils/env$': '<rootDir>/src/__mocks__/env.ts'
   },
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', {
-      tsconfig: 'tsconfig.test.json',
-    }],
-  },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  reporters: [
-    'default',
-    ['jest-html-reporter', {
-      pageTitle: 'Test Report',
-      outputPath: 'test-report.html',
-      includeFailureMsg: true,
-      includeConsoleLog: true,
-    }],
-  ],
   testPathIgnorePatterns: [
     '/node_modules/',
     'src/components/DebateGame.test.tsx'
